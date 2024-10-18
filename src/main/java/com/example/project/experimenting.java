d2e9613003b068b785720698b84258b19a987508
GavinY8 <gaviny8@nycstudents.net>
2024-10-18 00:49:02 +0000

    u9hqwuowq

---
+++ src/main/java/com/example/project/experimenting.java
@@ -0,0 +1,8 @@
+package com.example.project;
+
+public class experimenting {
+    public static void main(String args[]) {
+        LinearCalculator calc = new LinearCalculator("(10,5)","(6,-1)");
+        System.out.println(calc.printInfo());
+    }
+}
